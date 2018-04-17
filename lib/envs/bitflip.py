a7896728adebf1fa4b7f6ec1aea89ddc26b6b010
Vlad Sandulescu <vlad>
2018-04-17 23:05:04 +0200

    Added goal to environment

--- lib/envs/bitflip.py
+++ lib/envs/bitflip.py
@@ -5,25 +5,23 @@ class BitFlipEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     np.random.seed(42)
 
-    def __init__(self, n, goal):
+    def __init__(self, n):
         self.n = n
-        self.goal = goal
-        self.done = False
         self.state = None
+        self.done = False
 
-    def _step(self, action):
+    def step(self, action, goal):
         if self.state is None:
             raise Exception('You must first initialize the environment by calling the reset method.')
 
         self.state[action] = 1 if self.state[action] == 0 else 0
-        self.done = np.array_equal(self.state, self.goal)
+        self.done = np.array_equal(self.state, goal)
         reward = -1 if not self.done else 0
-
         return self.state.copy(), reward, self.done, {}
 
-    def _reset(self):
-        self.state = np.random.choice([0, 1], size=(self.n))
+    def reset(self):
+        self.state = np.random.choice([0, 1], size=self.n)
         return self.state.copy()
 
-    def _render(self, mode='human', close=False):
+    def render(self, mode='human', close=False):
         pass

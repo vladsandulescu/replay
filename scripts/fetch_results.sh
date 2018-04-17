f8c6ce02b7c6793aaf42c08562a535a0689a1905
Vlad Sandulescu <vlad>
2018-04-17 23:04:28 +0200

    Added shell scripts for running on remote instance

---
+++ scripts/fetch_results.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export INSTANCE="10.22.33.8"
+echo $INSTANCE
+scp -i instance/ubuntu-main.key -r ubuntu@$INSTANCE:"/opt/sandbox/v.sandulescu/vol/replay/experiments/results.csv" ../experiments/

c1fc3e4eaa370a3edafc1fb1c6a4969f7dbf8313
Vlad Sandulescu <vlad>
2018-05-04 15:08:51 +0200

    Return goal when resetting environment

--- lib/envs/bitflip.py
+++ lib/envs/bitflip.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gym
 
+
 class BitFlipEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     np.random.seed(42)
@@ -8,20 +9,22 @@ class BitFlipEnv(gym.Env):
     def __init__(self, n):
         self.n = n
         self.state = None
+        self.goal = None
         self.done = False
 
-    def step(self, action, goal):
-        if self.state is None:
+    def step(self, action):
+        if self.state is None or self.goal is None:
             raise Exception('You must first initialize the environment by calling the reset method.')
 
         self.state[action] = 1 if self.state[action] == 0 else 0
-        self.done = np.array_equal(self.state, goal)
+        self.done = np.array_equal(self.state, self.goal)
         reward = -1 if not self.done else 0
         return self.state.copy(), reward, self.done, {}
 
     def reset(self):
         self.state = np.random.choice([0, 1], size=self.n)
-        return self.state.copy()
+        self.goal = np.random.choice([0, 1], size=self.n)
+        return self.state.copy(), self.goal.copy()
 
     def render(self, mode='human', close=False):
         pass
